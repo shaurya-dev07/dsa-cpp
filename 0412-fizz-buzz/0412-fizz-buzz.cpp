e87a4402266ef39609a2a09a9dc594f92326f6ca
Shaurya Pratap Singh <shaurya.pratap.singh.dev@gmail.com>
2026-09-01 13:55:42 +0530

    Time: 3 ms (31.04%), Space: 11.8 MB (48.57%) - LeetHub - Time: O(n), Space: O(n)

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -7,12 +7,15 @@ public:
         for(int i=1; i<=n; i++){
             if(i%3==0 && i%5==0){
                 ans.push_back("FizzBuzz");
+                continue;
             }
             else if(i%3==0){
                 ans.push_back("Fizz");
+                continue;
             }
             else if(i%5==0){
                 ans.push_back("Buzz");
+                continue;
             }
             else{
                 ans.push_back(to_string(i));
