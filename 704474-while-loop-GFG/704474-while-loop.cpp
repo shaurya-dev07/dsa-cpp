36d21715926d0c4e650db80a352da350b660cc3e
Shaurya Pratap Singh <shaurya.pratap.singh.dev@gmail.com>
2026-09-02 11:09:21 +0530

    Force committed solution - Time: O(x), Space: O(1)

---
+++ 704474-while-loop-GFG/704474-while-loop.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+
+    // code here
+    while(x>=0){
+        cout << x << " ";
+        x--;
+    }
+
+    return 0;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
