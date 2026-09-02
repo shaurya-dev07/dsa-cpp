49d31dfaf1141c4b168ae574347c82440c806286
Shaurya Pratap Singh <shaurya.pratap.singh.dev@gmail.com>
2026-09-02 09:35:30 +0530

    Time: 0 ms (100%), Space: 30.3 MB (36.43%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
