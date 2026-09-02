35fb4e80d8a3d99972e4cb554c5a6a62923e65ce
Shaurya Pratap Singh <shaurya.pratap.singh.dev@gmail.com>
2026-09-02 11:08:32 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 700947-multiplication-table-GFG/700947-multiplication-table.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // code here
+    for(int i=1; i<=10; i++){
+        cout << n*i << " ";
+    }
+
+    return 0;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
