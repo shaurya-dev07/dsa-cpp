a2120d1025bfa67c9c6f538950adaae1bd814cde
Shaurya Pratap Singh <shaurya.pratap.singh.dev@gmail.com>
2026-09-02 11:23:16 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 703724-odd-or-even-GFG/703724-odd-or-even.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    bool isEven(int n) {
+        // code here
+        if(n%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
