b699ecbe6e6bf7f7f5894e53796e53bb42265020
Shaurya Pratap Singh <shaurya.pratap.singh.dev@gmail.com>
2026-09-02 11:21:38 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 704983-sum-of-natural-numbers-GFG/704983-sum-of-natural-numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // code here
+    int sum =0;
+    for(int i=0; i<=n; i++){
+        sum+=i;
+    }
+    cout << sum;
+    return 0;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
